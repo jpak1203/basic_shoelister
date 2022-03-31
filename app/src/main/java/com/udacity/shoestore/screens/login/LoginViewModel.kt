50555706ac8d5967e88c702af0c8b6a31e128408
jpak1203 <jpak1203@gmail.com>
2022-03-31 11:26:54 -0400

    navigation fix

--- app/src/main/java/com/udacity/shoestore/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/screens/login/LoginViewModel.kt
@@ -10,11 +10,23 @@ class LoginViewModel: ViewModel() {
     val showWelcomeScreen: LiveData<Boolean>
         get() = _showWelcomeScreen
 
+    private val _showShoeListings = MutableLiveData<Boolean>()
+    val showShoeListings: LiveData<Boolean>
+        get() = _showShoeListings
+
     fun showWelcomeScreen() {
         _showWelcomeScreen.value = true
     }
 
     fun onWelcomeScreenComplete() {
         _showWelcomeScreen.value = false
+    }
+
+    fun showShoeListings() {
+        _showShoeListings.value = true
+    }
+
+    fun onShoeListingsComplete() {
+        _showShoeListings.value = false
     }
 }
